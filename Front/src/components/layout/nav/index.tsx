e48ec8c072e516ea7716f0c0b7168a70283e8039
ferhanih <hicham.ferhani@etu.umontpellier.fr>
2025-07-21 17:10:13 +0100

    feat : burger menu

--- Front/src/components/layout/nav/index.tsx
+++ Front/src/components/layout/nav/index.tsx
@@ -8,39 +8,30 @@ export default function Nav() {
                 <Link to="/" className="flex items-center space-x-3 rtl:space-x-reverse">
                     <img
                         src={imga}
-                        className="h-auto sm:h-16" // garde la hauteur responsive, mais laisse la largeur naturelle
+                        className="h-auto sm:h-16 " // garde la hauteur responsive, mais laisse la largeur naturelle
                         alt="arganisme Cosmetics Logo"
                     />
                 </Link>
-
-                <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-                    <Link
-                        to="/login"
-                        className="block py-2 px-3 text-black rounded-sm hover:text-emerald-700 md:p-0"
-                    >
-                        Log in
-                    </Link>
-                    <button
-                        data-collapse-toggle="navbar-sticky"
-                        type="button"
-                        className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500
+                <button
+                    data-collapse-toggle="navbar-sticky"
+                    type="button"
+                    className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500
                                    rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2
                                    focus:ring-gray-200"
-                        aria-controls="navbar-sticky"
-                        aria-expanded="false"
-                    >
-                        <span className="sr-only">Open main menu</span>
-                        <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
-                            <path
-                                stroke="currentColor"
-                                strokeLinecap="round"
-                                strokeLinejoin="round"
-                                strokeWidth="2"
-                                d="M1 1h15M1 7h15M1 13h15"
-                            />
-                        </svg>
-                    </button>
-                </div>
+                    aria-controls="navbar-sticky"
+                    aria-expanded="true"
+                >
+                    <span className="sr-only">Open main menu</span>
+                    <svg className="w-5 h-5" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 17 14">
+                        <path
+                            stroke="currentColor"
+                            strokeLinecap="round"
+                            strokeLinejoin="round"
+                            strokeWidth="2"
+                            d="M1 1h15M1 7h15M1 13h15"
+                        />
+                    </svg>
+                </button>
 
                 <div className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1" id="navbar-sticky">
                     <ul className="flex flex-col p-4 md:p-0 mt-4 font-medium border border-gray-100 rounded-lg
@@ -78,6 +69,16 @@ export default function Nav() {
                             >
                                 Contact
                             </Link>
+                        </li>
+                        <li>
+                            <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
+                                <Link
+                                    to="/login"
+                                    className="block py-2 px-3 text-black rounded-sm hover:text-emerald-700 md:p-0"
+                                >
+                                    Log in
+                                </Link>
+                            </div>
                         </li>
                     </ul>
                 </div>
