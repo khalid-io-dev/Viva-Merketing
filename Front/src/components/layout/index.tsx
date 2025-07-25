17c7f64ec539752b3ba3d1cad6a6af059da37b2f
ferhanih <hicham.ferhani@etu.umontpellier.fr>
2025-07-25 18:52:49 +0100

    feat : amélioration Home page

--- Front/src/components/layout/index.tsx
+++ Front/src/components/layout/index.tsx
@@ -1,11 +1,13 @@
 import Navbar from '../layout/nav/index.tsx';
 import { Footer } from './footer';
 
+
+const isHomePage = location.pathname === '/';
 const Layout: React.FC<{ children: React.ReactNode }> = ({ children }) => (
 
     <div className="flex flex-col min-h-screen max-w-8xl mx-auto overflow-x-hidden bg-gray-50">
         <Navbar />
-        <main className="flex flex-col flex-grow pt-24 w-full">
+        <main className={isHomePage ? "flex flex-col flex-grow w-full" : "flex flex-col flex-grow pt-20 w-full"}>
             {children}
         </main>
         <Footer />
--- Front/src/pages/public/home/index.tsx
+++ Front/src/pages/public/home/index.tsx
@@ -1,11 +1,15 @@
-import AboutUs from "../about";
-import Presentation from "../presentation";
+import Presentation from "../../../components/presentation";
+import Services from "../../../components/services";
+import WhyUs from "../../../components/WhyUs";
+import Organisations from "../../../components/organisations";
 
 export default function Home() {
     return (
         <div className="flex flex-col gap-0">
-            <Presentation/>
-            <AboutUs/>
+            <Presentation />
+            <Services />
+            <WhyUs />
+            <Organisations />
         </div>
             )
 }
