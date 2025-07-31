0441694758e030139b458172531c55412457437a
alid404 <chainsawman.11294@hotmail.com>
2025-07-31 14:22:49 +0100

    merge issues

---
+++ Front/postcss.config.js
@@ -0,0 +1,12 @@
+// postcss.config.js
+import postcssImport from 'postcss-import';
+import tailwindcss from 'tailwindcss';
+import autoprefixer from 'autoprefixer';
+
+export default {
+  plugins: [
+    postcssImport,
+    tailwindcss,
+    autoprefixer
+  ]
+};
